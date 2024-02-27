5fde292af11d5e247183a2fabd849d6568d1ed4b
Atsuko Yamaguchi <atsuko.yamaguchi@gmail.com>
2024-02-28 00:25:22 +0900

    Create Cataract.py

---
+++ Cataract.py
@@ -0,0 +1,10 @@
+import pathlib
+import textwrap
+import google.generativeai as genai
+
+import vertexai
+from vertexai.preview.generative_models import GenerativeModel, Part
+
+import time
+import requests
+
